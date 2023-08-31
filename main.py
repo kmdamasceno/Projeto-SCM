7c4baa7a772d4bc9176ff763a6ad1bf28b406fb3
Thyago Chrystiann <thyago.chrystiann@hotmail.com>
2023-08-30 23:52:04 -0300

    Implementação de classe e função para criação de conta bancária, e inclusão de arquivo .data para armazenamento de contas.

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
+import pickle
+import os
+import pathlib
+
+
+ACCOUNTS_DATA_PATH = "accounts.data"
+NEW_ACCOUNTS_DATA_PATH = "new_accounts.data"
+
+
+class Account:
+    def __init__(self):
+        self.accNo = 0
+        self.name = ""
+        self.deposit = 0
+        self.type = ""
+
+    def createAccount(self):
+        self.accNo = int(input("\n\tEnter the account number: "))
+        self.name = input("\tEnter the account holder name: ")
+        self.type = input("\tEnter the type of account [C/S]: ")
+        self.deposit = int(
+            input("\tEnter the initial amount (>=500 for Saving and >=1000 for current): "))
+        print("\n\n\n\tAccount Created.")
+
+
+def writeAccount():
+    account = Account()
+    account.createAccount()
+    writeAccountsFile(account)
+
+def writeAccountsFile(account):
+    file = pathlib.Path(ACCOUNTS_DATA_PATH)
+    if file.exists():
+        infile = open(ACCOUNTS_DATA_PATH, "rb")
+        oldlist = pickle.load(infile)
+        oldlist.append(account)
+        infile.close()
+        os.remove(ACCOUNTS_DATA_PATH)
+    else:
+        oldlist = [account]
+    outfile = open(NEW_ACCOUNTS_DATA_PATH, "wb")
+    pickle.dump(oldlist, outfile)
+    outfile.close()
+    os.rename(NEW_ACCOUNTS_DATA_PATH, ACCOUNTS_DATA_PATH)
+
+
 if __name__ == "__main__":
     print("\n")
     print("\t========================")
@@ -20,7 +66,7 @@ if __name__ == "__main__":
 
         if ch == "1":
             print("\n")
-            pass
+            writeAccount()
         elif ch == "2":
             num = int(input("\n\tEnter the account number: "))
             pass
